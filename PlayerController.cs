60cd4a1d2094d9891c1827203b34eee8c99bf5c9
unknown <connor.checkley@gmail.com>
2020-03-29 11:54:06 +0100

    added comments

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 
+// Ensure the component is present on the gameobject the script is attached to
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    // Local rigidbody variable to hold a reference to the attached Rigidbody2D component
     new Rigidbody2D rigidbody2D;
 
     public float movementSpeed = 1000.0f;
 
     void Awake()
     {
+        // Setup Rigidbody for frictionless top down movement and dynamic collision
         rigidbody2D = GetComponent<Rigidbody2D>();
 
         rigidbody2D.isKinematic = false;
@@ -18,12 +21,15 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // Handle user input
         Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
         Move(targetVelocity);
     }
 
     void Move(Vector2 targetVelocity)
     {        
-        rigidbody2D.velocity = (targetVelocity * movementSpeed) * Time.deltaTime;
+        // Set rigidbody velocity
+        rigidbody2D.velocity = (targetVelocity * movementSpeed) * Time.deltaTime; // Multiply the target by deltaTime to make movement speed consistent across different framerates
     }
 }
