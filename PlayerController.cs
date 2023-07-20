db0e0d4574b9b6c5cd7a83f3da3308094f3ce087
Connor <connor.checkley@gmail.com>
2023-07-20 17:23:35 +0100

    Fixed outstanding issues
    Issue #1 - Multiplying Velocity Directly
    Issue #2 - Non normalized input Vector

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,35 +1,34 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 // Ensure the component is present on the gameobject the script is attached to
-[RequireComponent(typeof(Rigidbody2D))]
+// Uncomment this if you want to enforce the object to require the RB2D component to be already attached
+// [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Local rigidbody variable to hold a reference to the attached Rigidbody2D component
-    new Rigidbody2D rigidbody2D;
-
-    public float movementSpeed = 1000.0f;
+    public Vector2 MovementSpeed = new Vector2(100.0f, 100.0f); // 2D Movement speed to have independant axis speed
+    private new Rigidbody2D rigidbody2D; // Local rigidbody variable to hold a reference to the attached Rigidbody2D component
+    private Vector2 inputVector = new Vector2(0.0f, 0.0f);
 
     void Awake()
     {
         // Setup Rigidbody for frictionless top down movement and dynamic collision
-        rigidbody2D = GetComponent<Rigidbody2D>();
-
-        rigidbody2D.isKinematic = false;
+        // If RequireComponent is used uncomment the GetComponent and comment the AddComponent out
+        // rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        rigidbody2D = gameObject.AddComponent<Rigidbody2D>();
+        
         rigidbody2D.angularDrag = 0.0f;
         rigidbody2D.gravityScale = 0.0f;
     }
 
     void Update()
     {
-        // Handle user input
-        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-        Move(targetVelocity);
+        inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
 
-    void Move(Vector2 targetVelocity)
-    {        
-        // Set rigidbody velocity
-        rigidbody2D.velocity = (targetVelocity * movementSpeed) * Time.deltaTime; // Multiply the target by deltaTime to make movement speed consistent across different framerates
+    void FixedUpdate()
+    {
+        // Rigidbody2D affects physics so any ops on it should happen in FixedUpdate
+        // See why here: https://learn.unity.com/tutorial/update-and-fixedupdate#
+        rigidbody2D.MovePosition(rigidbody2D.position + (inputVector * MovementSpeed * Time.fixedDeltaTime));
     }
 }
